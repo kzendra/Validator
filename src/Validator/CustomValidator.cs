3230ce338b7765d265c3ccd0ce002f249b29107c
kzendra <drazen@kzendra.com>
2022-03-17 07:27:15 +0100

    Updated code (#1)
    * Updated code
    
    * Update

---
+++ src/Validator/CustomValidator.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace ValidatorTest
+{
+    public class CustomValidator : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var type = value.GetType();
+            if (type.IsEnum)
+            {
+                return ValidateEnum(value, validationContext);
+            }
+            else
+                return ValidationResult.Success;
+        }
+
+        public static ValidationResult ValidateEnum(object value, ValidationContext validationContext)
+        {
+            var type = value.GetType();
+            if (type.GetCustomAttributes<FlagsAttribute>().Any())
+                throw new NotImplementedException();
+            else
+            {
+                if (!Enum.IsDefined(type, value))
+                    return new ValidationResult($"The value {value} is not an option for {validationContext.DisplayName} objects.");
+                else
+                    return ValidationResult.Success;
+            }
+        }
+    }
+}
